bc6c6c09d0de8ef544a121645859d0d419973ac3
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Air.local>
2025-12-24 20:58:50 +0530

    Initial commit: add Java project structure and first introduction program

---
+++ src/com/learning/basics/Introduction.java
@@ -0,0 +1,19 @@
+package com.learning.basics;
+
+public class Introduction {
+    public static void main(String [] args) {
+        // THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+         */
+        System.out.println("Built different.");
+        System.out.println("Locked in 🔒");
+        System.out.println("Too smooth with it.");
+    }
+
+}
